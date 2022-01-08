12cfdfc3d59b9f941585e6bcfa72012b6a00d18f
Finite <remieluri@gmail.com>
2022-01-07 23:37:07 -0500

    Minor changes

---
+++ Blade/CodeAnalysis/Syntax/AdvanceToMemberExpressionSyntax.cs
@@ -0,0 +1,16 @@
+﻿namespace Blade.CodeAnalysis.Syntax
+{
+    public sealed class AdvanceToMemberExpressionSyntax : ExpressionSyntax
+    {
+        public AdvanceToMemberExpressionSyntax(SyntaxToken dotToken, ExpressionSyntax memberExpression)
+        {
+            DotToken = dotToken;
+            MemberExpression = memberExpression;
+        }
+
+        public override SyntaxKind Kind => SyntaxKind.AdvanceToMemberExpression;
+
+        public SyntaxToken DotToken { get; }
+        public ExpressionSyntax MemberExpression { get; }
+    }
+}
---
+++ Blade/CodeAnalysis/Syntax/ArrayInitializerExpressionSyntax.cs
@@ -0,0 +1,19 @@
+﻿namespace Blade.CodeAnalysis.Syntax
+{
+    public sealed class ArrayInitializerExpressionSyntax : ExpressionSyntax
+    {
+        public ArrayInitializerExpressionSyntax(SyntaxToken openBracketToken, SeparatedSyntaxList<ArrayElementSyntax> arrayElements, SyntaxToken closeBracketToken, TypeClauseSyntax typeClauseSyntax)
+        {
+            OpenBracketToken = openBracketToken;
+            ArrayElements = arrayElements;
+            CloseBracketToken = closeBracketToken;
+            TypeClauseSyntax = typeClauseSyntax;
+        }
+
+        public override SyntaxKind Kind => SyntaxKind.ArrayInitializerExpression;
+        public SyntaxToken OpenBracketToken { get; }
+        public SeparatedSyntaxList<ArrayElementSyntax> ArrayElements { get; }
+        public SyntaxToken CloseBracketToken { get; }
+        public TypeClauseSyntax TypeClauseSyntax { get; }
+    }
+}
